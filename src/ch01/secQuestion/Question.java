ce788f660c0940a149250cb0f1f6b2dafef4d56f
Junsu An <junsu6752@gmail.com>
2022-12-02 19:07:31 +0900

    Java start!

---
+++ src/ch01/secQuestion/Question.java
@@ -0,0 +1,19 @@
+package ch01.secQuestion;
+
+public class Question {
+
+	public static void main(String[] args) {
+		/*
+		 1. 4
+		 2. 2?
+		 3. 4?
+		 4. 3
+		 5. 3 1 4 2  -> 3 1 2 4
+		 6. 4
+		 7. 4  ->  올바른 것: 1, 3
+		 8.
+		 */
+
+	}
+
+}
