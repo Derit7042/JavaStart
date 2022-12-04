33d438f347ee98d013c19442fd3e7aac094091b9
user <user@DESKTOP-QP8EH36>
2022-12-04 21:02:27 +0900

    2장까지 완료

---
+++ src/ch02/secQuestion/Question.java
@@ -0,0 +1,23 @@
+package ch02.secQuestion;
+
+public class Question {
+
+	public static void main(String[] args) {
+		/*
+		 답지 86페이지
+		 1. 4
+		 2. 3, 4, 5, 6  ->  2, 3, 6, 7
+		 3. byte, short, char, int, long, float, double, boolean
+		 4. int, double, age, price, 10, 3.14
+		 5. 3
+		 6. 3  ->  4
+		 7. 4  ->  3
+		 8. 3?
+		 9. 3?  ->  1
+		 10. 2
+		 11. 13, 16 
+		 */
+
+	}
+
+}
